8c53c6175c51569045619312cd9e9e68ca81395b
Vladyslav Kolomoiets <vlad161200@gmail.com>
2021-01-19 21:58:39 +0200

    Add DELETE item

--- routers/itemsRouter.js
+++ routers/itemsRouter.js
@@ -134,5 +134,37 @@ router.put('/:id', async (req, res) => {
     }
 });
 
+router.delete('/:id', async (req, res) => {
+    try {
+        const decodedToken = jwt.decodeToken(req.headers.authorization);
+        const connection = mysqlConfig.connection();
+        await connection.query(`SELECT items.id, items.created_at, items.title,
+            items.price, items.image, items.user_id, users.phone,
+            users.name, users.email FROM items JOIN users ON items.user_id=users.id 
+            WHERE items.id = ${req.params.id}`, async (error, result) => {
+            if (error) {
+                console.log(error);
+            } else {
+                if (!result.length) {
+                    return res.status(404).json({});
+                } else {
+                    if (decodedToken.id === result[0].user_id && decodedToken.email === result[0].email) {
+                        await connection.query(`DELETE FROM items WHERE id = ${req.params.id}`, (error, result) => {
+                            if (error) {
+                                console.error(error)
+                            } else {
+                                return res.status(200).json({});
+                            }
+                        })
 
+                    } else {
+                        return res.status(403).json({});
+                    }
+                }
+            }
+        });
+    } catch (error) {
+        return res.status(401).json({});
+    }
+});
 module.exports = router;
