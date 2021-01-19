bb8ef1192ae1136553ce0b335da36fb7ee5458c7
Vladyslav Kolomoiets <vlad161200@gmail.com>
2021-01-19 21:17:44 +0200

    Realize PUT(update) of item

--- routers/itemsRouter.js
+++ routers/itemsRouter.js
@@ -1,11 +1,13 @@
 const {Router} = require('express');
 const router = Router();
+
 const mysqlConfig = require('../tools/mysql');
+const jwt = require('../tools/jwt');
 
 router.get('/', async (req, res) => {
     const connection = mysqlConfig.connection();
     await connection.query(`SELECT items.id, items.created_at, items.title, 
-    items.price, items.image, items.user_id, users.id, users.phone,
+    items.price, items.image, items.user_id, users.id as "user_id", users.phone,
      users.name, users.email FROM items JOIN users ON items.user_id=users.id`, (error, result) => {
             if (error) {
 
@@ -38,11 +40,13 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     const connection = mysqlConfig.connection();
     await connection.query(`SELECT items.id, items.created_at, items.title, 
-    items.price, items.image, items.user_id, users.id, users.phone,
-     users.name, users.email FROM items JOIN users ON items.user_id=users.id WHERE items.id = ${req.params.id}`, (error, result) => {
+    items.price, items.image, items.user_id, users.id as "user_id", users.phone,
+     users.name, users.email FROM items JOIN users ON items.user_id=users.id 
+     WHERE items.id = ${req.params.id}`, (error, result) => {
             if (error) {
 
             } else {
+                console.log(result);
                 if (result.length) {
                     let returnResult = {
                         id: result[0].id,
@@ -67,5 +71,68 @@ router.get('/:id', async (req, res) => {
         }
     );
 });
+
+router.put('/:id', async (req, res) => {
+    try {
+        const decodedToken = jwt.decodeToken(req.headers.authorization);
+        const connection = mysqlConfig.connection();
+        await connection.query(`SELECT items.id, items.created_at, items.title,
+            items.price, items.image, items.user_id, users.phone,
+            users.name, users.email FROM items JOIN users ON items.user_id=users.id 
+            WHERE items.id = ${req.params.id}`, async (error, result) => {
+            if (error) {
+                console.log(error);
+            } else {
+                if (!result.length) {
+                    return res.status(404).json({});
+                } else {
+                    if (decodedToken.id === result[0].user_id && decodedToken.email === result[0].email) {
+                        await connection.query(`UPDATE ITEMS SET 
+                        title = "${req.body.title || result[0].title}", 
+                        price = ${req.body.price || result[0].price}, 
+                        image = "${req.body.image || result[0].image}"
+                         WHERE id = ${req.params.id};`,
+                            async (error, resultOfUpdate) => {
+                                if (error) {
+                                    console.log(error);
+                                } else {
+                                    await connection.query(`SELECT items.id, items.created_at, items.title,
+                                         items.price, items.image, items.user_id, users.phone,
+                                         users.name, users.email FROM items JOIN users ON items.user_id=users.id 
+                                         WHERE items.id = ${req.params.id}`, (error, result) => {
+                                        if (error) {
+                                            console.error(error)
+                                        } else {
+                                            let itemAfterUpdate = {
+                                                id: result[0].id,
+                                                created_at: result[0].created_at,
+                                                title: result[0].title,
+                                                price: result[0].price,
+                                                image: result[0].image,
+                                                user_id: result[0].user_id,
+                                                user: {
+                                                    id: result[0].user_id,
+                                                    phone: result[0].phone,
+                                                    name: result[0].name,
+                                                    email: result[0].email
+                                                }
+                                            };
+                                            return res.status(200).json(itemAfterUpdate);
+                                        }
+                                    });
+                                }
+                            });
+
+                    } else {
+                        return res.status(403).json({});
+                    }
+                }
+            }
+        });
+    } catch (error) {
+        return res.status(401).json({});
+    }
+});
+
 
 module.exports = router;
